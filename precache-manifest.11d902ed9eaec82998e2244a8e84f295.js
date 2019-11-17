0ccd3010b74d4b084b806e2cd5a2d69684cc5062
IhorKalyta <kalyta.ihor@gmail.com>
2019-11-17 19:32:16 +0200

    Updates

---
+++ precache-manifest.11d902ed9eaec82998e2244a8e84f295.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b816cb68ec1ea682aa3dfba055fe3876",
+    "url": "/Dairy-App/index.html"
+  },
+  {
+    "revision": "aae910423924463550b1",
+    "url": "/Dairy-App/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "b4f7dd6f52b2a752805d",
+    "url": "/Dairy-App/static/css/main.c7c275da.chunk.css"
+  },
+  {
+    "revision": "aae910423924463550b1",
+    "url": "/Dairy-App/static/js/2.4cca6840.chunk.js"
+  },
+  {
+    "revision": "b4f7dd6f52b2a752805d",
+    "url": "/Dairy-App/static/js/main.8954e80f.chunk.js"
+  },
+  {
+    "revision": "3bec84652a8164ad300f",
+    "url": "/Dairy-App/static/js/runtime-main.828a2314.js"
+  }
+]);
